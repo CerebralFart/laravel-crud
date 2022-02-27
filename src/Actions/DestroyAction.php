8a98ec22ccb9621b3fb85acb8dd780a502572ac5
Sven Mol <s.mol@student.utwente.nl>
2022-02-27 12:52:35 +0100

    Ensure only used parameters are implicitly bound in RouteHelper

--- src/Actions/DestroyAction.php
+++ src/Actions/DestroyAction.php
@@ -5,7 +5,6 @@ namespace Cerebralfart\LaravelCRUD\Actions;
 use Illuminate\Http\Request;
 
 // TODO [0.1.1] Allow customizing the response
-// TODO [0.1.x] Redirect response should not include the ID of the removed user
 trait DestroyAction {
     public function destroy(Request $request) {
         $instance = $this->resolveModel($request);
--- src/Helpers/RouteHelper.php
+++ src/Helpers/RouteHelper.php
@@ -3,6 +3,7 @@
 namespace Cerebralfart\LaravelCRUD\Helpers;
 
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 
@@ -11,8 +12,16 @@ trait RouteHelper {
         $current = Route::current();
         $prefix = Str::beforeLast($current->getName(), '.');
         $route = $prefix . '.' . $suffix;
-        return redirect()->route($route, array_merge(
+
+        // Only copy over the parameters which are used by the new route
+        // Prevents nasty URLs like /users?user=42 when going from .edit to .index
+        $implicitParameters = Arr::only(
             $current->parameters,
+            Route::getRoutes()->getByName($route)->parameterNames(),
+        );
+
+        return redirect()->route($route, array_merge(
+            $implicitParameters,
             $data
         ));
     }
