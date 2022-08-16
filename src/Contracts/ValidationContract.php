5599d0198df5fc58b37a6456205c119177ba3f67
Sven Mol <s.mol@student.utwente.nl>
2022-08-16 13:38:21 +0200

    Touch up in validation contract php-doc

--- src/Contracts/ValidationContract.php
+++ src/Contracts/ValidationContract.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 interface ValidationContract {
     /**
      * @param Model $model The model to be validated
-     * @param array $rules The developer-defined set of validation rules
-     * @return array The selected subset of validation rules
+     * @param array<string, string> $rules The developer-defined set of validation rules
+     * @return array<string, string> The selected subset of validation rules
      */
     function selectValidationRules(
         Model $model,
