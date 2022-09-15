6bbdc22e977400095edcfb58b4d26b5d96de0092
Sven Mol <s.mol@student.utwente.nl>
2022-09-15 16:55:59 +0200

    Update model name resolution logic

--- src/Helpers/ModelHelper.php
+++ src/Helpers/ModelHelper.php
@@ -5,10 +5,7 @@ namespace Cerebralfart\LaravelCRUD\Helpers;
 use Exception;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\MessageBag;
 use Illuminate\Support\Str;
 
 trait ModelHelper {
@@ -17,21 +14,31 @@ trait ModelHelper {
     /** @var null|string|array<int, string> The name to be used when exposing this model to views */
     public $modelName = null;
 
-    protected function resolveModelName(Request $request, bool $plural): string {
-        if (is_array($this->modelName)) {
-            return $this->modelName[$plural ? 1 : 0];
+    public function guessModelName(Request $request):string{
+        return Str::lower(Str::of($this->model)
+            ->afterLast('\\')
+            ->split('/(?=[A-Z])/')
+            ->last());
+    }
+
+    public function resolveModelName(Request $request, bool $plural): string {
+        // If no explicit model name has been defined, we guess it.
+        // The exact logic for this can be overridden via traits
+        if ($this->modelName === null) {
+            $this->modelName = $this->guessModelName($request);
         }
 
-        $str = is_string($this->modelName)
-            ? $this->modelName
-            : Str::lower(Str::of($this->model)
-                ->afterLast('\\')
-                ->split('/(?=[A-Z])/')
-                ->last());
+        // If the model name is a string, we conjugate it and store it for later use
+        // This is also done with the results from the guessModelName function
+        if (is_string($this->modelName)) {
+            $this->modelName = [
+                Str::singular($this->modelName),
+                Str::plural($this->modelName),
+            ];
+        }
 
-        return $plural
-            ? Str::plural($str)
-            : Str::singular($str);
+        // By this point, the modelName attribute should be an array
+        return $this->modelName[$plural ? 1 : 0];
     }
 
     protected function resolveModelId(Request $request): string {
